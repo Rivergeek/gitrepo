30062888d9ee1fe1512f12828928dbdc3de9a88f
Rivergeek <kazanov.j.s@gmail.com>
2018-10-25 13:02:41 +0600

    ++

--- helli.py
+++ helli.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import psutil          #сторонний модуль
 
 name = input("Введите_имя: ")
@@ -8,11 +9,14 @@ if quest == "д":
     print('Что будем делать?')
     print('1 - Вывести список файлов')
     print('2 - Вывести информацию о системе')
+    print('3 - Номер текущего процесса')
     deis = int(input("Укажите номер действи: "))
     if deis == 1:
         print(os.listdir())
     elif deis == 2:
-        pass
+        print(psutil.pids())
+    elif deis == 3:
+        print(os.getpid())
     else:
         pass
 elif quest == "н":
